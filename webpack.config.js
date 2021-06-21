1465798dad127ba33f18ba22159664e6343e73ef
Jeferson Velásquez <jvelasquezc15@miumg.edu.gt>
2021-06-21 10:00:22 -0600

    04 - loader para CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -26,7 +27,17 @@ module.exports = {
             loader: 'html-loader'
           }
         ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader'
+        ]
       }
+      
     ]
   },
   plugins: [
@@ -34,10 +45,14 @@ module.exports = {
       template: './public/index.html',
       filename: './index.html'
     }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
+    })
   ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 3005,
+    open: true,
   }
 };
